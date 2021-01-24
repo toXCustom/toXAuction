5a6e4fb50ff4e69693c6b1400e97084ffcb60c6b
Paweł Mróz <pawel89@gmail.com>
2021-01-24 14:37:47 +0100

    Start of the App toXCustomAuction

---
+++ src/main/java/com/toXCustom/toXAuction/ToXAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.toXCustom.toXAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToXAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToXAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toXCustom/toXAuction/ToXAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toXCustom.toXAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToXAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
